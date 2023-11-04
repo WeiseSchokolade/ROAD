8c92b7ff3dfde12e48f12f64d6160a38a4d08436
WeiseSchokolade <99491985+WeiseSchokolade@users.noreply.github.com>
2023-11-04 02:10:40 +0100

    Removed map selection from multiplayer menu

--- src/de/schoko/road/multiplayer/MultiPlayerMenu.java
+++ src/de/schoko/road/multiplayer/MultiPlayerMenu.java
@@ -12,16 +12,12 @@ import de.schoko.road.MainMenu;
 import de.schoko.road.Menu;
 import de.schoko.road.RoadProject;
 import de.schoko.road.TextButton;
-import de.schoko.road.server.shared.SharedConstants;
 
 public class MultiPlayerMenu extends Menu {
 	private TextButton playButton;
-	private TextButton mapButton;
 	private TextButton backButton;
 	private CarSelectionButton carSelectionButton;
 	private String message;
-	
-	private int currentMap;
 	
 	public MultiPlayerMenu() {
 	}
@@ -36,7 +32,6 @@ public class MultiPlayerMenu extends Menu {
 		context.getSettings().setBackgroundColor(59, 59, 59);
 		
 		playButton = new TextButton(context, "Play", 5, 0, Color.RED, Constants.MAIN_MENU_FONT, Constants.MAIN_MENU_BACKGROUND_COLOR, Constants.MAIN_MENU_BACKGROUND_COLOR, Color.WHITE, Color.LIGHT_GRAY);
-		mapButton = new TextButton(context, "Map: " + SharedConstants.MAP_NAMES[currentMap], 0, 0, Color.RED, Constants.MAIN_MENU_FONT, Constants.MAIN_MENU_BACKGROUND_COLOR, Constants.MAIN_MENU_BACKGROUND_COLOR, Color.WHITE, Color.LIGHT_GRAY);
 		backButton = new TextButton(context, "Back", 5, 0, Color.RED, Constants.MAIN_MENU_FONT, Constants.MAIN_MENU_BACKGROUND_COLOR, Constants.MAIN_MENU_BACKGROUND_COLOR, Color.WHITE, Color.LIGHT_GRAY);
 		carSelectionButton = new CarSelectionButton(context);
 	}
@@ -44,13 +39,7 @@ public class MultiPlayerMenu extends Menu {
 	@Override
 	public void update(double deltaTime) {
 		if (playButton.wasReleased()) {
-			RoadProject.get().setMenu(new ConnectionMenu(SharedConstants.MAP_NAMES[currentMap]));
-			return;
-		}
-		if (mapButton.wasReleased()) {
-			currentMap++;
-			if (currentMap >= SharedConstants.MAP_NAMES.length) currentMap = 0;
-			mapButton.setText("Map: " + SharedConstants.MAP_NAMES[currentMap]);
+			RoadProject.get().setMenu(new ConnectionMenu());
 			return;
 		}
 		if (backButton.wasReleased()) {
@@ -71,10 +60,6 @@ public class MultiPlayerMenu extends Menu {
 			hud.drawText(message, hud.getWidth() / 2, hud.getHeight() / 4 + playButton.getHeight() + Constants.MAIN_MENU_SMALL_FONT.getSize(),
 					Color.RED, Constants.MAIN_MENU_SMALL_FONT, TextAlignment.CENTER);
 		}
-		
-		mapButton.setX((int) (hud.getWidth() / 4), TextAlignment.CENTER);
-		mapButton.setY((int) (hud.getHeight() / 2));
-		hud.draw(mapButton);
 		
 		backButton.setY((int) (hud.getHeight() - backButton.getHeight() - 6));
 		hud.draw(backButton);
